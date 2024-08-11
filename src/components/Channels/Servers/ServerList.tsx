70338b3dbab04133a9ec09c95dfcc328d67450fe
sabashatirishvili <shatirishvilis1@gmail.com>
2024-08-11 21:54:04 +0400

    Added Sidebar

--- src/components/Channels/Servers/ServerList.tsx
+++ src/components/Channels/Servers/ServerList.tsx
@@ -1,17 +1,25 @@
 import React from 'react'
 import Server from './Server'
 import AddServer from './AddServer'
+import { IoIosChatboxes } from "react-icons/io";
+import { IoChatboxSharp } from "react-icons/io5";
 
 export default function ServerList() {
   return (
     <div className='bg-slate-900 shadow-md flex flex-col px-2 justify-between items-center py-4'>
-      <div className='flex flex-col justify-start gap-3 py-2 border-b-2 grow border-slate-600 mb-4'>
-        <Server/>
-        <Server/>
-        <Server/>
-        <Server/>
+      <div className='pb-4 border-b border-slate-700'>
+        <div className='bg-slate-800 p-4 rounded-full flex justify-center items-center *:grow text-green-600'>
+          <IoChatboxSharp />
+        </div>
       </div>
-      <AddServer/>
+
+      <div className='flex flex-col justify-start gap-3 pb-2 pt-4 border-b-2 grow border-slate-600 mb-4'>
+        <Server />
+        <Server />
+        <Server />
+        <Server />
+      </div>
+      <AddServer />
     </div>
   )
 }
--- src/components/Channels/Sidebar/User.tsx
+++ src/components/Channels/Sidebar/User.tsx
@@ -14,11 +14,11 @@ export default function User() {
   const onMouseLeaveHandler = () => {
     setHover(prev => !prev)
   }
-  
+
   return (
-    <div onMouseEnter={onMouseEnterHandler} onMouseLeave={onMouseLeaveHandler} className='flex items-center justify-between hover:bg-slate-700 px-2 text-sm rounded-sm hover:cursor-pointer'>
-      <div className='flex items-center  py-1 gap-2'>
-        <div className='flex justify-center items-center p-4 rounded-full bg-orange-500 text-center'>
+    <div onMouseEnter={onMouseEnterHandler} onMouseLeave={onMouseLeaveHandler} className=' flex items-center justify-between hover:bg-slate-700 px-2 text-sm rounded-sm hover:cursor-pointer'>
+      <div className='flex items-center  py-1 gap-3'>
+        <div className='flex justify-center items-center p-4 rounded-full bg-green-600 text-center'>
           <h1 className='absolute'>U</h1>
         </div>
         <h3>the_user</h3>
