aeb73d9fd88220b37c4899302159fb2aa2ea2057
sabashatirishvili <shatirishvilis1@gmail.com>
2024-08-17 09:46:19 +0400

    Added animations

--- src/components/Servers/AddServer.tsx
+++ src/components/Servers/AddServer.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { motion } from "framer-motion";
 import { FaPlus } from "react-icons/fa";
 
 export default function AddServer() {
   return (
-    <div className='bg-slate-800 p-4 rounded-full cursor-pointer'>
-      <FaPlus className='text-green-600'/>
-    </div>
+    
+    <motion.div className='bg-slate-800 p-4 rounded-full cursor-pointer'>
+      <motion.div>
+        <FaPlus className='text-green-600' />
+      </motion.div>
+    </motion.div>
   )
 }
