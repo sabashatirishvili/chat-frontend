4d1395b02a8873f4f2040bfc515f9c67aaf03c56
sabashatirishvili <shatirishvilis1@gmail.com>
2024-08-17 23:25:34 +0400

    Channels page done

--- src/app/channels/[id]/page.tsx
+++ src/app/channels/[id]/page.tsx
@@ -1,15 +1,38 @@
+import React from 'react'
 import ChatList from '@/components/Channels/ChatList'
 import Header from '@/components/Chats/Header'
-import React from 'react'
+import InputBar from '@/components/Chats/InputBar'
+import Message from '@/components/Message/Message'
+import { MdAlternateEmail } from 'react-icons/md'
 
 function Channel() {
   return (
     <div className='flex grow'>
       <div className='h-full'>
-        <ChatList /> j
+        <ChatList />
       </div>
-      <div className='grow'>
-        <Header/>
+      <div className='flex flex-col grow'>
+        <Header />
+        <div className='flex flex-col grow overflow-auto'>
+          <div className='flex flex-col grow'>
+            <div className='mt-[12rem] flex flex-col gap-4 items-center justify-center text-slate-700'>
+              <MdAlternateEmail size={128} className='text-slate-900' />
+              <div className='flex flex-col items-center text-center'>
+                <h1 className='text-2xl'>You have no messages with the_user yet.</h1>
+                <h3 className='text-lg'>You may start a conversation!</h3>
+              </div>
+            </div>
+            <Message />
+            <Message />
+            <Message />
+            <Message />
+            <Message />
+            <Message />
+          </div>
+          <div>
+          </div>
+        </div>
+        <InputBar />
       </div>
     </div>
 
