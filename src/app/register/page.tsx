eb83e58d0d75ced2bfb0643de60d4651be432154
sabashatirishvili <shatirishvilis1@gmail.com>
2024-08-23 23:24:05 +0400

    Login page works

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,22 +1,36 @@
 "use client";
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios';
 import Link from 'next/link';
 import AuthInput from '@/components/Authentication/AuthInput';
 import { GoogleLogin } from '@react-oauth/google';
 
 function Login() {
+  const [credentials, setCredentials] = useState({
+    username: "",
+    email: "",
+    password: ""
+  })
+
+  const handleChange = (field: string, value: string) => {
+    setCredentials(prev => ({ ...prev, [field]: value }))
+  }
+  console.log(credentials)
+
   return (
     <div className='w-full h-screen flex justify-center items-center'>
       <div className='w-full rounded-lg flex *:flex-1 bg-slate-900 border border-slate-700 max-w-[64rem] py-6 px-2'>
         <div className='flex flex-col border-r border-slate-700 p-4 gap-6'>
           <h1 className='text-center text-lg pb-6 border-b border-slate-700'>Sign in</h1>
           <div className='flex flex-col gap-2'>
-            <AuthInput label="Email" />
-            <AuthInput label="Username" />
-            <AuthInput label="Password" password={true} />
+            <AuthInput label="Email" onChange={handleChange} />
+            <AuthInput label="Username" onChange={handleChange} />
+            <AuthInput label="Password" password={true} onChange={handleChange} />
           </div>
           <Link href="/login" className='text-gray-300  underline'>Already have an account? Sign in</Link>
-          <button className='self-center bg-green-600 text-gray-200 py-1 px-4 rounded-sm text-md'>Submit</button>
+          <button onClick={() => {
+            axios.post('http://localhost:8000/api/users/login/', credentials).then((res) => console.log(res.data))
+          }} className='self-center bg-green-600 text-gray-200 py-1 px-4 rounded-sm text-md'>Submit</button>
         </div>
         <div className='flex flex-col p-4'>
           <h1 className='text-center text-lg pb-6 border-b border-slate-700'>Social</h1>
--- src/components/Authentication/AuthInput.tsx
+++ src/components/Authentication/AuthInput.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
 
 interface PropTypes {
-  label: String,
-  password?: boolean
+  label: string,
+  password?: boolean,
+  data?: string,
+  onChange: Function
 }
-export default function AuthInput({ label, password }: PropTypes) {
+export default function AuthInput({ label, password, onChange }: PropTypes) {
   return (
     <div className='flex flex-col gap-[2px]'>
       <label htmlFor="password" className='text-md text-gray-300'>{label}</label>
-      <input type={password ? 'password' : 'text'} className='px-2 py-2 rounded-sm bg-slate-800' />
+      <input onChange={(e) => {
+        onChange(label.toLowerCase(), e.target.value);
+      }} type={password ? 'password' : 'text'} className='px-2 py-2 rounded-sm bg-slate-800' />
     </div>
   )
 }
